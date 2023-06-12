3825533d86aeffaffd03690511907070ecb01b91
dejmedus <email96123@gmail.com>
2023-06-12 11:36:12 -0600

    allow custom categories

--- src/extension.ts
+++ src/extension.ts
@@ -8,24 +8,13 @@ import { ICategories } from "./lib/types";
 let disposable: vscode.Disposable | undefined;
 
 export function activate(context: vscode.ExtensionContext) {
-  // get categories list
-  const categories: ICategories = sortConfig.categories;
-
-  // get config from workspace or use default sort order
+  // get config from workspace or use default properties and sort order
   const config = vscode.workspace.getConfiguration("css-style-sorter");
-  const sortOrder: string[] = config.get("sortOrder", [
-    "*",
-    "Box Model",
-    "Grid",
-    "Flexbox",
-    "Width and Height",
-    "Margin and Padding",
-    "Border",
-    "Typography",
-    "Background and Visual",
-    "Transformations and Animations",
-    "Other",
-  ]);
+  const categories: ICategories = config.get(
+    "categories",
+    sortConfig.categories
+  );
+  const sortOrder: string[] = config.get("sortOrder", sortConfig.order);
 
   // on save inside css files, sort CSS
   let disposable = vscode.workspace.onWillSaveTextDocument((event) => {
@@ -33,7 +22,7 @@ export function activate(context: vscode.ExtensionContext) {
       const text = event.document.getText();
       const sortedCSS = sortCSS(text, sortOrder, categories);
 
-      // on willSave + waitUntil prevents looping
+      // onWillSave + waitUntil prevents looping
       event.waitUntil(
         Promise.resolve([
           new vscode.TextEdit(
