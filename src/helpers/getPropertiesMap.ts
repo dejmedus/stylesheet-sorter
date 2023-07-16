71113363d124791ffeaef968bb21a209ceb1950a
dejmedus <email96123@gmail.com>
2023-07-15 22:30:11 -0600

    get sort order from workspace config

--- src/helpers/getPropertiesMap.ts
+++ src/helpers/getPropertiesMap.ts
@@ -1,9 +1,43 @@
+import * as vscode from "vscode";
 import { ICategories } from "../lib/types";
+import * as sortConfig from "../lib/sort.config.json";
 
-export default function getPropertiesMap(
-  sortOrder: string[],
-  categories: ICategories
-) {
+export default function getPropertiesMap() {
+  // get config from workspace or use default properties and sort order
+  const config = vscode.workspace.getConfiguration("CSS Style Sorter");
+  let categories: ICategories = config.get("Categories", sortConfig.categories);
+  let sortOrder: string[] = config.get(
+    "Sort Order",
+    sortConfig.order
+  ).sortOrder;
+
+  // ensure valid config files - categories in sort order must exist in categories list
+  let validConfig = Object.keys(categories).length === sortOrder.length;
+
+  if (validConfig) {
+    for (let i = 0; i < sortOrder.length; i++) {
+      if (!Object.keys(categories).includes(sortOrder[i])) {
+        validConfig = false;
+        console.error(
+          "CSS Style Sorter: No category ",
+          sortOrder[i],
+          " found."
+        );
+
+        break;
+      }
+    }
+  }
+
+  // if config is invalid, use defaults
+  if (!validConfig) {
+    console.error(
+      "CSS Style Sorter: Invalid configuration. Please check sort order in settings. Using default sort order."
+    );
+    categories = sortConfig.categories;
+    sortOrder = sortConfig.order.sortOrder;
+  }
+
   let propertiesMap: { [property: string]: number } = {};
   let index = 0;
   for (let i = 0; i < sortOrder.length; i++) {
