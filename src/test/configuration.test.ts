43bf168293bc1d324deb856e809f453d5331c27a
Julia <email96123@gmail.com>
2025-01-01 15:45:44 -0700

    tests

--- src/test/configuration.test.ts
+++ src/test/configuration.test.ts
@@ -2,7 +2,7 @@ import * as vscode from "vscode";
 import * as assert from "assert";
 import * as sinon from "sinon";
 
-import getPropertiesMap from "../helpers/getPropertiesMap";
+import getPropertiesMap from "../getPropertiesMap";
 
 suite("VS Code Configuration", () => {
   let getConfigurationStub: sinon.SinonStub;
@@ -13,5 +13,33 @@ suite("VS Code Configuration", () => {
 
   teardown(() => {
     getConfigurationStub.restore();
+  });
+
+  test("getPropertiesMap respects custom sort order and categories", () => {
+    getConfigurationStub.returns({
+      get: (configName: string) => {
+        if (configName === "categories") {
+          return {
+            customCategory1: ["customClass1", "customClass2"],
+            customCategory2: ["customClass3", "customClass4"],
+          };
+        }
+        if (configName === "sortOrder") {
+          return ["customCategory2", "customCategory1"];
+        }
+        return {};
+      },
+    });
+
+    const propertiesMap = getPropertiesMap();
+
+    const expectedPropertiesMap = {
+      customClass1: 2,
+      customClass2: 3,
+      customClass3: 0,
+      customClass4: 1,
+    };
+
+    assert.deepStrictEqual(propertiesMap, expectedPropertiesMap);
   });
 });
